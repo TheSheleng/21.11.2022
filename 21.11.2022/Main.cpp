92517f685a037a07773f2019fdaa9bbaa1f0a4a6
The_Sheleng <sheleng55@gmail.com>
2022-11-21 10:57:33 +0200

    Добавьте файлы проекта.

---
+++ 21.11.2022/Main.cpp
@@ -0,0 +1,17 @@
+#define FIRSTDLL_API extern "C" __declspec(dllexport)
+
+#include "Main.h"
+
+FIRSTDLL_API const double Pi = 3.1415;
+
+
+FIRSTDLL_API int fooInDll()
+{
+	int a = 100;
+	return a;
+}
+
+FIRSTDLL_API int barInDll()
+{
+	return 0;
+}
---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,38 @@
+﻿#pragma comment (lib, "test1")
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+#include "../test1/Main.h"
+
+int main()
+{
+	while (true)
+	{
+		float a, b;
+		cout << "Enter first num: ";
+		cin >> a;
+		cout << "Enter seccond num: ";
+		cin >> b;
+		cout << endl;
+
+		cout << "Chose action: " << endl
+			<< "\t1. Sum" << endl
+			<< "\t2. Min" << endl
+			<< "\t3. Mult" << endl
+			<< "\t4. Div" << endl;
+
+		switch (_getch())
+		{
+		case '1': cout << Sum(a, b); break;
+		case '2': cout << Min(a, b); break;
+		case '3': cout << Mult(a, b); break;
+		case '4':
+			try { cout << Div(a, b); }
+			catch (const char* s) { cout << "Error: " << s; }
+			break;
+		}
+
+		cout << endl << endl;
+	}
+}
